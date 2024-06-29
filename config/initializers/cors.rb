2e174a35ad486ce6c2ce6486215e04491ddedea5
Andy Borsz <andy.borsz@gmail.com>
2024-06-29 12:05:32 -0400

    Configure CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins "https://chatem-frontend.vercel.app/", "http://localhost:5173/"
 
-    resource '*',
+    resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
