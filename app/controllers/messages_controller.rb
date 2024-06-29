547479b37544e5a21085641f8a4f78890db465fb
Andy Borsz <andy.borsz@gmail.com>
2024-06-29 11:56:41 -0400

    Render created message json

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
     if message.save
       chat_room = ChatRoom.find(message.chat_room_id)
       ChatRoomChannel.broadcast_to(chat_room, message)
-      # render json: message
+      render json: message
     else
       render json: {errors: message.errors.full_messages}, status: 422
     end
