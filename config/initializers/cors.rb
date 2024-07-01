5eb921519daa6a01493aadb5fe360a01eabf7449
Andy Borsz <andy.borsz@gmail.com>
2024-07-01 15:41:44 -0400

    Add browserless.io info to cors middleware

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,13 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://chatem-frontend.vercel.app", "http://localhost:5173", "http://localhost:3000", "https://evening-headland-99649-ad144f8efa52.herokuapp.com"
+    origins(
+      "https://chatem-frontend.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://evening-headland-99649-ad144f8efa52.herokuapp.com",
+      "production-sfo.browserless.io",
+      "production-lon.browserless.io"
+    )
 
     resource "*",
       headers: :any,
